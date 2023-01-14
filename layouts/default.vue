16bea4ddbc13f046081c13b8b8fa1c90820093dd
David Waring <dev@davidwaring.net>
2023-01-14 03:18:20 -0500

    Move Recommendations component

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,9 +7,9 @@
     <Toolbar class="fixed top-0 left-0 w-full z-30" />
     <Alert class="fixed inset-x-0 bottom-0 sm:bottom-2 z-40"/>
     <main :class="{'pt-14': route.path !== '/'}">
-      <slot />
+      <slot></slot>
     </main>
-    <Footer class="mt-auto" />
+    <Footer class="mt-auto"></Footer>
   </div>
 </template>
 
