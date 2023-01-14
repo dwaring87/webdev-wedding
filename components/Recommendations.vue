fcdcf6c0aca919fe8b0de0b760399a8719c4c2b1
David Waring <dev@davidwaring.net>
2023-01-14 18:52:16 -0500

    Improve Recommendation UI

--- components/Recommendations.vue
+++ components/Recommendations.vue
@@ -27,25 +27,31 @@
 
         <!-- Recommendation Card -->
         <Card v-for="(rec) in recommendations[category]" :title="rec.name" class="recommendation">
-          <div class="flex flex-wrap md:flex-nowrap gap-4 items-center">
-            <div class="max-w-full lg:max-w-sm">
+          <div class="flex flex-wrap md:flex-nowrap gap-4">
+            <div class="max-w-full lg:max-w-fit">
               <p v-if="rec.location">
                 <strong>Location:</strong>&nbsp;
                 <a :href="`https://www.google.com/maps/search/?api=1&query=${rec.location}`" target="_blank">{{ rec.location }}</a>
               </p>
               <p v-if="rec.website && rec.website.length > 0">
                 <strong>Website:</strong>&nbsp;
-                <span class="inline-block mr-4" v-for="(w) in rec.website"><a :href="w.url" target="_blank">{{ w.title }}</a></span>
+                <span v-for="(w, i) in rec.website" class="inline-block">
+                  <span v-if="i !== 0">&ensp;&bull;&ensp;</span>
+                  <a :href="w.url" target="_blank">{{ w.title }}</a>
+                </span>
               </p>
               <p v-if="rec.map && rec.map.length > 0">
                 <strong>Map:</strong>&nbsp;
-                <span class="inline-block mr-4" v-for="(m) in rec.map"><a :href="m.url" target="_blank">{{ m.title }}</a></span>
+                <span v-for="(m, i) in rec.map" class="inline-block">
+                  <span v-if="i !== 0">&ensp;&bull;&ensp;</span>
+                  <a :href="m.url" target="_blank">{{ m.title }}</a>
+                </span>
               </p>
               <br />
               <p> {{ rec.description }}</p>
             </div>
-            <div class="mx-auto">
-              <Image :d-id="rec.image" class="mx-auto w-max max-h-42 md:max-h-48 lg:max-w-sm lg:max-h-fit" width="400px" />
+            <div class="mx-auto my-auto">
+              <Image :d-id="rec.image" class="mx-auto w-max max-h-42 md:max-h-48 lg:max-h-60 lg:max-w-60" style="margin-top: 0px; margin-bottom: 0px;" width="400px" />
             </div>
           </div>
         </Card>
