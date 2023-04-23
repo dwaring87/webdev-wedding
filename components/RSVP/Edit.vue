3e447a25de903f3a223e9aee9df36202d6bd2405
David Waring <dev@davidwaring.net>
2023-04-23 08:34:18 -0400

    RSVP: Properly submit nos

--- components/RSVP/Edit.vue
+++ components/RSVP/Edit.vue
@@ -59,8 +59,8 @@
       const updated_guest_props = {
         name: guest.name !== original.name ? guest.name : undefined,
         email: guest.email !== original.email ? guest.email : undefined,
-        rsvp_welcome: guest.rsvp_welcome !== original.rsvp_welcome ? guest.rsvp_welcome : undefined,
-        rsvp: guest.rsvp !== original.rsvp ? guest.rsvp : undefined,
+        rsvp_welcome: guest.rsvp_welcome,
+        rsvp: guest.rsvp,
         dietary_restrictions: JSON.stringify(guest.dietary_restrictions) !== JSON.stringify(original.dietary_restrictions) ? guest.dietary_restrictions : undefined,
         notes: guest.notes !== original.notes ? guest.notes : undefined
       }
